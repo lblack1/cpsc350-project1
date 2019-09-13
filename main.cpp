f788fd02a0c1dbc8bb826b48a14dd0edea542bfc
lblack1 <49578072+lblack1@users.noreply.github.com>
2019-09-12 23:59:48 -0700

    Preliminary upload
    Contains read, statistics, and write functionalities
    Lacks Gaussian distribution strings and any significant commenting

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "proj1.h"
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  DNAReader test = DNAReader();
+  test.WriteStats();
+}
---
+++ proj1.h
@@ -0,0 +1,97 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+
+using namespace std;
+
+class DNAReader {
+
+  public:
+    DNAReader();
+    DNAReader(string fileName);
+
+    int ComputeTotals();
+    float ComputeMean();
+    float ComputeVariance();
+    float ComputeStddev();
+
+    float ComputeRelA();
+    float ComputeRelC();
+    float ComputeRelT();
+    float ComputeRelG();
+    float ComputeRelAA();
+    float ComputeRelAC();
+    float ComputeRelAT();
+    float ComputeRelAG();
+    float ComputeRelCA();
+    float ComputeRelCC();
+    float ComputeRelCT();
+    float ComputeRelCG();
+    float ComputeRelTA();
+    float ComputeRelTC();
+    float ComputeRelTT();
+    float ComputeRelTG();
+    float ComputeRelGA();
+    float ComputeRelGC();
+    float ComputeRelGT();
+    float ComputeRelGG();
+
+    void WriteStats();
+
+  private:
+    string ifilename;
+    string ofilename;
+    string nucleodump;
+
+    int totalLines;
+    int totalA;
+    int totalC;
+    int totalT;
+    int totalG;
+    int totalAA;
+    int totalAC;
+    int totalAT;
+    int totalAG;
+    int totalCA;
+    int totalCC;
+    int totalCT;
+    int totalCG;
+    int totalTA;
+    int totalTC;
+    int totalTT;
+    int totalTG;
+    int totalGA;
+    int totalGC;
+    int totalGT;
+    int totalGG;
+
+    float relA;
+    float relC;
+    float relT;
+    float relG;
+    float relAA;
+    float relAC;
+    float relAT;
+    float relAG;
+    float relCA;
+    float relCC;
+    float relCT;
+    float relCG;
+    float relTA;
+    float relTC;
+    float relTT;
+    float relTG;
+    float relGA;
+    float relGC;
+    float relGT;
+    float relGG;
+
+    int sum;
+    int totalbigrams;
+    float mean;
+    float variance;
+    float stddev;
+
+};
